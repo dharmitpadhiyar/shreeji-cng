79007f1645306898f6f0ef17510607a2e8718cae
dharmitpadhiyar <dharmitpadhiyar222@gmail.com>
2025-07-07 19:28:03 +0530

    Update: Added new services

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -1,7 +1,7 @@
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/9316096209" // <-- replace with your number
+      href="https://wa.me/9376763635" // <-- replace with your number
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-0 right-4  text-white p-6 rounded-full shadow-lg "
