c977b3b99b67f6c841cf7061a120ff0e9f2a3037
dharmitpadhiyar <dharmitpadhiyar222@gmail.com>
2025-07-07 19:56:38 +0530

    Update: Added new services

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,7 @@ export default function Contact() {
       <div className="mt-8">
         <h2 className="text-2xl font-bold mb-4 text-center">Our Location</h2>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3353.063315890269!2d72.13010027473452!3d21.77036736189581!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395f50876578daed%3A0xbe5123fe5f2bc722!2sShreeji%20Service!5e1!3m2!1sen!2sin!4v1751698678818!5m2!1sen!2sin"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1676.511928051421!2d72.13312016211381!3d21.772055633640143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395f51b5469acf97%3A0x300d258c24eb04af!2sShreeji%20CNG%20Retesting%20Center!5e1!3m2!1sen!2sin!4v1751898295158!5m2!1sen!2sin"
           width="100%"
           height="300"
           allowFullScreen=""
