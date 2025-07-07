71b62fe7726e3490761cf5640a9f796223c4d952
dharmitpadhiyar <dharmitpadhiyar222@gmail.com>
2025-07-07 21:35:10 +0530

    Your update message

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -4,7 +4,7 @@ export default function WhatsAppButton() {
       href="https://wa.me/9376763635" // <-- replace with your number
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-0 right-4 p-6 rounded-full shadow-lg "
+      className="fixed bottom-0 right-4 p-6 "
     >
       <img src="/whatsapp-icon.jpeg" alt="WhatsApp" className="p-4 w-16 h-16" />
     </a>
